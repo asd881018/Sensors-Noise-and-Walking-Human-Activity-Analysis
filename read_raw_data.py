2be47e5c5ece17bc58c2dd6617ae683ff05eac84
Sidak Singh <sidaksingh@Sidaks-MacBook-Air.local>
2021-11-29 21:16:47 -0800

    Removed a file name typo

--- read_raw_data.py
+++ read_raw_data.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
 ## process_file() takes a string with a path to the .csv
 def process_file(pathname):
@@ -25,11 +26,11 @@ def main():
     # 5 minutes waking data- Matt
     # Right pocket
     mrp_5min = process_file('data/matt_rp50_5mins.csv')
-    # mlp_5min = process_file('data/matt_lp50_5mins.csv')
-    mrp_5min = mrp_5min[(mrp_5min['seconds'] >= 30) & (mrp_5min['seconds'] <= 330)]
-    # mlp_5min = mlp_5min[(mlp_5min['seconds'] >= 30) & (mlp_5min['seconds'] <= 330)]
+    mlp_5min = process_file('data/matt_lp50_5mins.csv')
+    mrp_5min = mrp_5min[(mrp_5min['seconds'] >= 10) & (mrp_5min['seconds'] <= 310)]
+    mlp_5min = mlp_5min[(mlp_5min['seconds'] >= 30) & (mlp_5min['seconds'] <= 330)]
 
-    # mlp_5min.to_csv('processed_data/mlp_5min.csv')
+    mlp_5min.to_csv('processed_data/mlp_5min.csv')
     mrp_5min.to_csv('processed_data/mrp_5min.csv')
 
 
@@ -37,13 +38,11 @@ def main():
     # Right pocket
     srp_5min = process_file('data/sam_rp50_5mins.csv')
     slp_5min = process_file('data/sam_lp50_5mins.csv')
-    srp_5min = srp_5min[(mrp_5min['seconds'] >= 30) & (srp_5min['seconds'] <= 330)]
+    srp_5min = srp_5min[(srp_5min['seconds'] >= 30) & (srp_5min['seconds'] <= 330)]
     slp_5min = slp_5min[(slp_5min['seconds'] >= 30) & (slp_5min['seconds'] <= 330)]
 
     slp_5min.to_csv('processed_data/slp_5min.csv')
     srp_5min.to_csv('processed_data/srp_5min.csv')
-
-    
 
 
 if __name__ == "__main__":
