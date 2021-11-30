f925f415002193f987ab0e678dc4350bd6cdbf84
Sidak Singh <sidaksingh@Sidaks-MacBook-Air.local>
2021-11-29 22:41:19 -0800

    read_raw_data.py now normalizes the seconds to 0

--- read_raw_data.py
+++ read_raw_data.py
@@ -29,6 +29,7 @@ def read_mrp_data():
     # Right pocket
     mrp_5min = process_file('data/matt_rp50_5mins_screeon.csv')
     mrp_5min = mrp_5min[(mrp_5min['seconds'] >= 10) & (mrp_5min['seconds'] <= 310)]
+    mrp_5min['seconds'] = mrp_5min['seconds'] - 10
     mrp_5min = mrp_5min.iloc[0:14000, :]
     return mrp_5min
 
@@ -37,6 +38,7 @@ def read_srp_data():
     # Right pocket
     srp_5min = process_file('data/sam_rp50_5mins.csv')
     srp_5min = srp_5min[(srp_5min['seconds'] >= 30) & (srp_5min['seconds'] <= 330)]
+    srp_5min['seconds'] = srp_5min['seconds'] - 30
     srp_5min = srp_5min.iloc[0:14000, :]
     return srp_5min
 
@@ -44,6 +46,7 @@ def read_slp_data():
     # Left pocket
     slp_5min = process_file('data/sam_lp50_5mins.csv')
     slp_5min = slp_5min[(slp_5min['seconds'] >= 30) & (slp_5min['seconds'] <= 330)]
+    slp_5min['seconds'] = slp_5min['seconds'] - 30
     slp_5min = slp_5min.iloc[0:14000, :]
     return slp_5min
 
@@ -51,21 +54,21 @@ def read_slp_data():
 def main():
 
     dlp_5min = read_dlp_data()
-    drp_5min = read_dlp_data()
+    drp_5min = read_drp_data()
 
     mrp_5min = read_mrp_data()
 
     slp_5min = read_slp_data()
     srp_5min = read_srp_data()
 
-    dlp_5min.to_csv('processed_data/dlp_5min.csv')
-    drp_5min.to_csv('processed_data/drp_5min.csv')
+    dlp_5min.to_csv('processed_data/dlp_5min.csv', index=False)
+    drp_5min.to_csv('processed_data/drp_5min.csv', index=False)
 
 
-    mrp_5min.to_csv('processed_data/mrp_5min.csv')
+    mrp_5min.to_csv('processed_data/mrp_5min.csv', index=False)
 
-    slp_5min.to_csv('processed_data/slp_5min.csv')
-    srp_5min.to_csv('processed_data/srp_5min.csv')
+    slp_5min.to_csv('processed_data/slp_5min.csv', index=False)
+    srp_5min.to_csv('processed_data/srp_5min.csv', index=False)
 
 if __name__ == "__main__":
     main()
